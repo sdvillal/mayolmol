7951105fdd17e63ed7cbbb21120c59a48920960a
santi <sdvillal@gmail.com>
2011-08-15 19:18:37 +0200

    Worked out all the cases in which openbabel exits abruptly (yet to handle all the non-depicted compounds)

--- mayolmol/scripts/dsstox_depict.py
+++ mayolmol/scripts/dsstox_depict.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-""" Generate spectrophores for the DSSTox datasets """
-import multiprocessing
+""" Generate an image for each compound of a dataset.
+    Note that at the moment none of the tried alternatives is able to generate succesfully
+    an image of all compounds, so we stick with openbabel.
+"""
 import os
 import os.path as op
 import pybel
 import argparse
-import math
-import numpy
-from mayolmol.descriptors import ob
 from mayolmol.scripts.dsstox_prep import DEFAULT_DSSTOX_DIR
 
-def depicter(dataset):
+def depict(dataset):
     print 'Depicting %s' % dataset
     if not op.exists(dataset):
         print '\tSDF file does not exist, skipping...'
@@ -20,8 +19,13 @@ def depicter(dataset):
     if not op.exists(depictions_dir):
         os.makedirs(depictions_dir)
     for mol in pybel.readfile('sdf', dataset):
-        mol.removeh()
-        mol.write('svg', op.join(depictions_dir, mol.title +'.svg'), overwrite=True)
+        print 'Depicting: %s' %mol.title
+        #This fails, need to research more (probably related to the porecomputed conformation)
+        #  terminate called after throwing an instance of 'std::out_of_range'
+        #    what():  vector::_M_range_check
+        if not any(name in mol.title for name in ('LD50-train-2934', 'LD50-train-3306', 'LD50-train-5215', 'LD50-test-0954')):
+            mol.removeh()
+            mol.write('png', op.join(depictions_dir, mol.title +'.png'), overwrite=True)
     print 'Depicting %s done' % dataset
 
 if __name__ == '__main__':
@@ -44,6 +48,5 @@ if __name__ == '__main__':
     #######################################
     datasets = sorted([name for name in os.listdir(root) if op.isdir(op.join(root, name))])
     datasets = map(lambda dataset: op.join(root, dataset, dataset + '.sdf'), datasets)
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    pool.map(depicter, datasets)
-    pool.close()
+    for dataset in datasets:
+        depict(dataset)
