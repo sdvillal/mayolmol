b7b11f816df10700d367b0e48e8d17097560bf93
santi <sdvillal@gmail.com>
2011-08-15 11:14:04 +0200

    Catched segfault errors for some molecules in mutagenicity, reported

--- mayolmol/scripts/dsstox_prep.py
+++ mayolmol/scripts/dsstox_prep.py
@@ -66,7 +66,7 @@ def create_saliviewer_input(master_file, dest_file):
         for values in input:
             output.writerow(values)
 
-def prepare_dsstox_dataset(root, name, dest=None):
+def prepare_dsstox_dataset(root, name, dest=None, overwrite=False):
     """ This method bootstraps the analysis of DSSTox data.
        - Rename the compounds
        - Merge train/test
@@ -76,34 +76,48 @@ def prepare_dsstox_dataset(root, name, dest=None):
     """
     if not dest: dest = root
 
-    print 'Reading %s' % name
-    trainMols = list(pybel.readfile('sdf', op.join(root, name + '_training.sdf')))
-    testMols = list(pybel.readfile('sdf', op.join(root, name + '_prediction.sdf')))
+    dataset_root = op.join(dest, name)
+    dest_sdf = op.join(dataset_root, name + '.sdf')
+    if op.exists(dest_sdf) and not overwrite:
+        print '%s is already there and not overwriting requested' % dest_sdf
+        return
 
-    datasetRoot = op.join(dest, name)
-    print '\tCreating dataset root: %s' % datasetRoot
-    if not op.exists(datasetRoot):
-        os.makedirs(datasetRoot)
+    print 'Reading %s' % name
+    train_mols = list(pybel.readfile('sdf', op.join(root, name + '_training.sdf')))
+    test_mols = list(pybel.readfile('sdf', op.join(root, name + '_prediction.sdf')))
+
+    print '\tCreating dataset root: %s' % dataset_root
+    if not op.exists(dataset_root):
+        os.makedirs(dataset_root)
 
     print '\tRenaming the compounds to keep track of the provenance'
-    rename_mols_by_index(trainMols, name + '-train-')
-    rename_mols_by_index(testMols, name + '-test-')
+    rename_mols_by_index(train_mols, name + '-train-')
+    rename_mols_by_index(test_mols, name + '-test-')
 
     print '\tGenerating conformations'
-    for mol in trainMols + testMols:
-        mol.make3D()
+    for mol in train_mols + test_mols:
+        #Some molecules from mutagenicity produce segfault on make3D
+        #See bug report at https://sourceforge.net/tracker/?func=detail&aid=3374324&group_id=40728&atid=428740
+        #Train 3988: OC(=O)[C@]1(C)CCC[C@]2(C1CC[C@]13C2CC[C@](C3)([C@]2(C1)OC2)O)C
+        #Train 4205: CC(CCC[C@H]([C@H]1CC[C@@H]2[C@]1(C)CC[C@H]1[C@H]2CC2([C@@H]3[C@]1(C)CC[C@@H](C3)Br)S(=O)(=O)CCS2(=O)=O)C)C
+        #This kind of fatal errors are worrying, is there any robust way of controlling them in python/java? Will need to create one
+        if not any(name in mol.title for name in ('train-3988', 'train-4205')):
+            try:
+                print 'Conformation for %s' % mol.title
+                mol.make3D()
+            except Exception:
+                print 'Error computing a 3D conformation for %s' % mol.title
 
     print '\tSaving compounds'
-    destSDF = op.join(datasetRoot, name + '.sdf')
-    save_mols(trainMols + testMols, destSDF)
+    save_mols(train_mols + test_mols, dest_sdf)
 
-    masterTable = op.join(datasetRoot, name + '-master.csv')
-    print '\tCreating \"master\" table: %s' % masterTable
-    create_master_table(destSDF, masterTable)
+    master_table = op.join(dataset_root, name + '-master.csv')
+    print '\tCreating \"master\" table: %s' % master_table
+    create_master_table(dest_sdf, master_table)
 
-    saliTable = op.join(datasetRoot, name + '-saliviewer.csv')
-    print '\tCreating \"saliviewer\" table: %s' % saliTable
-    create_saliviewer_input(masterTable, saliTable)
+    sali_table = op.join(dataset_root, name + '-saliviewer.csv')
+    print '\tCreating \"saliviewer\" table: %s' % sali_table
+    create_saliviewer_input(master_table, sali_table)
 
 if __name__ == '__main__':
     #######################################
@@ -152,3 +166,4 @@ if __name__ == '__main__':
 
     pool = multiprocessing.Pool(multiprocessing.cpu_count())
     pool.map(prepare, datasets)
+    pool.close()
