6a41051cf28cf51fe4dea2d0bdd279d6b1b78542
santi <sdvillal@gmail.com>
2011-08-23 00:02:33 +0200

    Got rid of spurious comments

--- mayolmol/descriptors/ob.py
+++ mayolmol/descriptors/ob.py
@@ -1,5 +1,4 @@
 """ Molecular descriptors using openbabel. """
-import pickle
 import pybel
 import numpy
 
@@ -8,14 +7,11 @@ def spectrophores(mols):
     Compute the spectrophores of the molecules and return a numpy array.
     We use default settings for the spectrophores.
     We assume the molecules have already 3D coordinates.
-    TODO: make robust / more defensive
     """
     spectromaker = pybel.ob.OBSpectrophore()
     specs = []
     for mol in mols:
         try:
-            print mol.title
-            #if mol.title != 'Density-train-6878':  #TODO: research into this molecule: OBSpectrophore::GetSpectrophore() error: not enough atoms in molecule
             specs.append(spectromaker.GetSpectrophore(mol.OBMol))
         except Exception, e:
             print 'failed to compute the spectrophore for mol %s' % mol.title
@@ -24,6 +20,6 @@ def spectrophores(mols):
     #Check if any spectrophore computation failed and if so fill the corresponding vector with NaNs
     for i, spec in enumerate(specs):
         if not len(spec):
-            specs[i] = [float('NaN')] * len(specs[0]) #TODO: Q&D, this fails if the first spec has failed itself, fix
+            specs[i] = [float('NaN')] * 48
 
     return numpy.array(specs)
