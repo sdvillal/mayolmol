e6c54198ecf5f92026ed1c9d6a880e41acaf7342
Floriane <fmontanari@irbbarcelona.org>
2011-09-23 14:26:23 +0200

    Now the

--- mayolmol/descriptors/ob.py
+++ mayolmol/descriptors/ob.py
@@ -20,11 +20,14 @@ def spectrophores(mols):
         except Exception, e:
             print 'failed to compute the spectrophore for mol %s' % mol.title
             print e
-
     #Check if any spectrophore computation failed and if so fill the corresponding vector with NaNs
     for i, spec in enumerate(specs):
         if len(spec) ==1:
+            print spec
             specs[i][1:] = [float('NaN')] * 48
-
-    return numpy.array(specs)
+    #a = [('id', numpy.str_, len(specs[0][0]))]
+    a = [('id', 'a'+str(len(specs[0][0])))]
+    for i in xrange(48):
+        a.append(('spec_' + str(i) , '<f8'))
+    return numpy.array(specs, dtype=a)
 
--- mayolmol/scripts/compute_properties.py
+++ mayolmol/scripts/compute_properties.py
@@ -5,19 +5,36 @@ import os.path as op
 import pybel
 import numpy
 import sys
-import mayolmol.descriptors.ob as spec
+from mayolmol.descriptors import ob as spec
 import mayolmol.descriptors.cdkdescui as cdkdescui
 
-def spectrophores(dataset):
+def spectrophores(dataset, overwrite=True):
     """ Compute the spectrophores for a dataset """
     try:
         print '\tSpectrophores'
         specs = spec.spectrophores(pybel.readfile('sdf', dataset))
         dataset_root, dataset_name = op.split(dataset)
         dataset_name = op.splitext(dataset_name)[0]
-        numpy.savetxt(op.join(dataset_root, dataset_name + '-ob-spectrophores.csv'),
-                      specs, fmt='%s' + ' %.6f'*48, delimiter=',')
-        print '\tSpectrophores computed succesfully'
+        #numpy.savetxt(op.join(dataset_root, dataset_name + '-ob-spectrophores.csv'),
+                      #specs, fmt='%s %.6f', delimiter=',')
+        destfile = op.join(dataset_root, dataset_name + '-ob-spectrophores.csv')
+        if op.exists(destfile) and overwrite==False:
+            print '%s is already there and not overwriting requested' % destfile
+            return
+        else:
+            if op.exists(destfile) and overwrite==True:
+                print '%s is already there but overwriting requested' % destfile
+                os.remove(destfile)
+            f= open(destfile, 'a')    
+            for molecule in specs:
+                f.write(molecule[0])
+                f.write(",")
+                for i in range(1,49):
+                    f.write(str(molecule[i]))
+                    f.write(",")
+                f.write("\n")
+            f.close()
+            print '\tSpectrophores computed succesfully'
     except Exception, e:
         print 'Damn, there has been a problem computing the spectrophores...'
         print 'Research into this...'
@@ -26,18 +43,18 @@ def spectrophores(dataset):
 if __name__ == '__main__':
     #CDKDescUI based properties
     #CDK_DEFAULT_TYPES = ['constitutional', 'geometric']
-    CDK_DEFAULT_DESCRIPTORS = op.join(op.split(__file__)[0], "../data/GuhaDescriptors")
-    CDK_DEFAULT_FINGERPRINTS = ['maccs', 'estate', 'extended']
+    #CDK_DEFAULT_DESCRIPTORS = op.join(op.split(__file__)[0], "../data/GuhaDescriptors")
+    #CDK_DEFAULT_FINGERPRINTS = ['maccs', 'estate', 'extended']
     dataset = sys.argv[1]
-    print 'Computing properties for %s' % dataset
-    cdkdescui.CDKDescUIDriver().compute_selection(dataset,
-                                                     op.splitext(dataset)[0] + '-cdk' + '.csv',
-                                                     CDK_DEFAULT_DESCRIPTORS,
-                                                     addH=True)
-    for fingerprint in CDK_DEFAULT_FINGERPRINTS:
-        print '\t' + fingerprint
-        cdkdescui.CDKDescUIDriver('java', "/mmb/pluto/fmontanari/Build/CDKDescUI.jar").compute_fingerprint(dataset,
-                                                            op.splitext(dataset)[0] + '-cdk-' + fingerprint + '.csv',
-                                                            fingerprint=fingerprint,
-                                                            addH=True)
+    #print 'Computing properties for %s' % dataset
+    #cdkdescui.CDKDescUIDriver().compute_selection(dataset,
+                                                     #op.splitext(dataset)[0] + '-cdk' + '.csv',
+                                                     #CDK_DEFAULT_DESCRIPTORS,
+                                                     #addH=True)
+    #for fingerprint in CDK_DEFAULT_FINGERPRINTS:
+        #print '\t' + fingerprint
+        #cdkdescui.CDKDescUIDriver('java', "/mmb/pluto/fmontanari/Build/CDKDescUI.jar").compute_fingerprint(dataset,
+                                                            #op.splitext(dataset)[0] + '-cdk-' + fingerprint + '.csv',
+                                                            #fingerprint=fingerprint,
+                                                            #addH=True)
     spectrophores(dataset)
