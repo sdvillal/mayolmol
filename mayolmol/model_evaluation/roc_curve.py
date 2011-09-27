42d3284645fca9640ba691ef2091a0fbc78bc7a0
Floriane <fmontanari@irbbarcelona.org>
2011-09-27 17:58:12 +0200

    Still buggy and some TODO to fill in

--- mayolmol/model_evaluation/roc_curve.py
+++ mayolmol/model_evaluation/roc_curve.py
@@ -3,6 +3,7 @@ import subprocess
 import os.path as op
 import time
 import croc
+import sys
 
 #Why don't we use the CROC API? -> we do now.
 
@@ -12,7 +13,9 @@ def prepare_input(directory, input_file, scoredlabel_file):
     g = open(op.join(directory, scoredlabel_file), 'a')
     for line in f:
         columns = line.split()
-        g.write(columns[1:])
+        for i in columns[1:]:
+            g.write(i)
+            g.write("\t")
         g.write("\n")
     f.close()
     g.close()
@@ -49,7 +52,48 @@ def compute_enrichment(directory, scoredlabel_file, curve_file):
     out.close()
  
 #print compute_auc_with_API("/mmb/pluto/fmontanari/Escritorio/testCROC", "coordinates.curve")
-#print compute_enrichment("/mmb/pluto/fmontanari/Escritorio/testCROC","input.scored-label", "enrichment.curve")        
-    
-        
+#print compute_enrichment("/mmb/pluto/fmontanari/Escritorio/testCROC","input.scored-label", "enrichment.curve")    
 
+def write_GNUplot_file(curve_type, directory, curve_file, params_file, image_file=None):
+    assert curve_type in ("roc", "enrichment")
+    #TODO: check of file. Delete any existing one
+    f = open(op.join(directory,params_file), 'a')
+    f.write("cd '" + directory + "'\n")
+    if image_file:
+        f.write("set terminal png\n")
+        f.write("set output '" + op.join(directory, image_file) + "'\n")
+    f.write("set key left box\n")
+    if curve_type == "roc":
+        f.write("set xlabel 'False positive rate'; set ylabel 'True positive rate';\n")
+        f.write("plot [0:1] '" + curve_file + "' with line title 'ROC of your model', x title 'ROC of a random classifier'\n")
+    else:
+        f.write("set xlabel 'Fraction of the ranked data tested'; set ylabel 'True positive rate'\n")
+        f.write("plot [0:1] '" + curve_file + "' with line title 'Enrichment curve of your model', x title 'Enrichment curve of a random classifier'\n")
+    if not image_file:
+        f.write("pause -1\n")
+    f.close()
+                
+def depict(directory, curve_type, curve_file, params_file, image_file=None):
+    write_GNUplot_file(curve_type, directory, curve_file, params_file, image_file)
+    args = "gnuplot '" + op.join(directory, params_file) + "'"
+    draw = subprocess.Popen(args, shell=True)
+    draw.wait()
+        
+if __name__ == "__main__":
+    #print depict("/mmb/pluto/fmontanari/Escritorio/testCROC", "enrichment", "enrichment.curve", "gnuplot.gp", "nicePlot.png")
+    #print write_GNUplot_file("roc", "/mmb/pluto/fmontanari/Escritorio/testCROC", "coordinates.curve", "gnuplot.gp")
+    #TODO: check input parameters
+    directory = sys.argv[1]
+    prediction_file = sys.argv[2]
+    model_name = sys.argv[3]
+    
+    #TODO checks of file and directory
+    scoredlabel_file = prepare_input(directory, prediction_file, model_name + ".scored-label")
+    compute_curve(directory, scoredlabel_file, model_name + ".roc")
+    compute_enrichment(directory, scoredlabel_file, model_name + ".enrich")
+    auc = compute_auc_with_API(directory, model_name + ".roc")
+    print "The Area Under the ROC is %.4f"%auc
+    depict(directory, "roc", model_name + ".roc", params + "_" + model_name + "_roc.gp", model_name + "_rocplot.png")
+    depict(directory, "enrichment", model_name + ".enrich", params + "_" + model_name + "_enrich.gp", model_name + "_enrichplot.png")
+    
+    
